eb9724096fd1765a1e7da354facd1c7d80507610
RohithLOL <ayyappan_rohith@students.edu.sg>
2025-03-27 14:10:45 +0000

    saveing test app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.title("Testing one two three")
+
+with st.container:
+    st.write(st.button(label="Quadratic equation",icon=":material/superscript:"))
+'''
+if quadratic == True:
+    st.text("it works!!")'
+'''
